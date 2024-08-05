987534aa59da7d55aad7391e432ee8294f146e05
kkarhin <123474101+kkarhin@users.noreply.github.com>
2024-08-05 13:01:06 +0300

    Update ecg_generator.py

--- ecg_generator.py
+++ ecg_generator.py
@@ -3,60 +3,67 @@ import numpy as np
 from signal_generator import SignalGenerator
 from noise_generator import NoiseGenerator
 from beat_interval_generator import BeatIntervalGenerator
-from scipy import signal
-from utils import create_label, default_field, min_max_normalize
+from utils import create_label, default_field
 
 @dataclass
-class PPGWavePrms:
-    fst: float=0.0
-    snd: float=0.0
+class ECGWavePrms:
+    p: float=0.0
+    q: float=0.0
+    r: float=0.0
+    s: float=0.0
+    t: float=0.0
     
     def to_list(self):
-        return [self.fst, self.snd]
-    
+        return [self.p, self.q, self.r, 
+                self.s, self.t]
+
 @dataclass
-class PPGGenerator(SignalGenerator):
+class ECGGenerator(SignalGenerator):
 
     noise_generator: NoiseGenerator = default_field(NoiseGenerator())
     beat_interval_generator: BeatIntervalGenerator = default_field(BeatIntervalGenerator())
     fs: int = 200
     number_of_beats: int = 30
-    ppg_amplitude: PPGWavePrms = default_field(PPGWavePrms(0.8, 0.8))
-    ppg_amplitude_low: PPGWavePrms = default_field(PPGWavePrms(0.5, 0.5))
-    ppg_amplitude_high: PPGWavePrms = default_field(PPGWavePrms(1.0, 0.9))
-    ppg_width: PPGWavePrms = default_field(PPGWavePrms(0.7, 1.9))
-    ppg_width_low: PPGWavePrms = default_field(PPGWavePrms(0.5, 1.7))
-    ppg_width_high: PPGWavePrms = default_field(PPGWavePrms(0.9, 2.1))
-    ppg_distance: PPGWavePrms = default_field(PPGWavePrms(-0.26, 0.11))
-    ppg_distance_low: PPGWavePrms = default_field(PPGWavePrms(-0.32, 0.06))
-    ppg_distance_high: PPGWavePrms = default_field(PPGWavePrms(-0.22, 0.16))
+    ecg_amplitude: ECGWavePrms = default_field(ECGWavePrms(0.1, -0.08, 1.0, -0.08, 0.3))
+    ecg_amplitude_low: ECGWavePrms = default_field(ECGWavePrms(0.05, -0.05, 0.8, -0.05, 0.1))
+    ecg_amplitude_high: ECGWavePrms = default_field(ECGWavePrms(0.2, -0.2, 1.2, -0.2, 0.6))
+    ecg_width: ECGWavePrms = default_field(ECGWavePrms(0.15, 0.1, 0.1, 0.1, 0.5))
+    ecg_width_low: ECGWavePrms = default_field(ECGWavePrms(0.065, 0.03, 0.06, 0.03, 0.085))
+    ecg_width_high: ECGWavePrms = default_field(ECGWavePrms(0.085, 0.08, 0.085, 0.08, 0.21))
+    ecg_distance: ECGWavePrms = default_field(ECGWavePrms(-0.12, -0.04, 0.0, 0.03, 0.25))
+    ecg_distance_low: ECGWavePrms = default_field(ECGWavePrms(-0.12, -0.03, 0.0, 0.03, 0.2))
+    ecg_distance_high: ECGWavePrms = default_field(ECGWavePrms(-0.18, -0.05, 0.0, 0.05, 0.25))
+    ecg_symmetry: ECGWavePrms = default_field(ECGWavePrms(1.0, 1.0, 1.0, 1.0, 3.0))
+    ecg_symmetry_low: ECGWavePrms = default_field(ECGWavePrms(1.0, 1.0, 1.0, 1.0, 1.0))
+    ecg_symmetry_high: ECGWavePrms = default_field(ECGWavePrms(1.0, 1.0, 1.0, 1.0, 5.0))
+
 
     def generate(self) -> tuple[np.ndarray, np.ndarray]:
         """
-        Generates clean or noisy PPG signal. Also returns labels for peaks and feet and for noise.
-
+        Generates clean or noisy ECG signals. Also returns indeces for P and T waves and R peaks and labels for noise.
+        
         Returns
         ----------
         signal
-            Clean or noisy PPG signal.
+            Clean or noisy ECG signal.
         peak_inds
-            1D array of labels for peaks and feet.
+            1D array of labels for P, R and T waves.
         noise_labels
-            Noise labels: list of tuples of noise type (str) and signal-length array of amplitude of noise. Last item in the list is the artifact label (array).
+            Noise labels: list of tuples of noise type (str) and signal-length array of amplitude of noise. Last tuple in the list is the artifact label (array).
         beat_intervals
             Beat intervals in seconds
         """
-
-        self.distance = self.ppg_distance.to_list()
-        self.width = self.ppg_width.to_list()
-        self.amplitude = self.ppg_amplitude.to_list()
-        self.symmetry = [1, 1]
+        
+        self.distance = self.ecg_distance.to_list()
+        self.width = self.ecg_width.to_list()
+        self.amplitude = self.ecg_amplitude.to_list()
+        self.symmetry = self.ecg_symmetry.to_list()
         noise_labels = []
 
         if self.noise_generator is not None:
             self.beat_interval_generator.duration = None
             self.noise_generator.fs = self.fs
-            noise_signal, noise_labels = self.noise_generator.generate()           
+            noise_signal, noise_labels = self.noise_generator.generate()     
 
             if self.noise_generator.noise_list:
                 dur = 0
@@ -70,40 +77,32 @@ class PPGGenerator(SignalGenerator):
         beat_intervals = self.beat_interval_generator.generate()
         if self.beat_interval_generator.duration is None:
             self.beat_interval_generator.duration = np.sum(beat_intervals)
-        signal_, beat_intervals = super().generate(beat_intervals, self.fs)
+        signal, beat_intervals = super().generate(beat_intervals, self.fs)
 
-        # Find peaks.
-        peaks, feet = [], []
-        for idx in np.cumsum(beat_intervals):
-            # Index range for finding the foot.
-            i1 = max(0, int(idx - 0.1 * self.fs))
-            i2 = min(np.sum(beat_intervals), int(idx + 0.1 * self.fs))
-            f = i1 + np.argmin(signal_[i1:i2])
-            feet.append(f)
-            # End index of the range for finding the peak.
-            i2 = min(np.sum(beat_intervals), int(f + 0.6 * self.fs))
-            # Find the relative maxima.
-            arg_rel_maxs = signal.argrelmax(signal_[f:i2])[0]
-            if len(arg_rel_maxs) > 0:
-                peaks.append(f + arg_rel_maxs[0])
-
-        peaks.insert(0, np.argmax(signal_[0:feet[0]]))
-        peak_inds = np.zeros(len(signal_))
-        peak_inds = create_label(peak_inds, np.array(peaks), 0.5)
-        peak_inds = create_label(peak_inds, np.array(feet), 0.25)
+        # Find R peaks.
+        r_peaks = np.zeros(len(beat_intervals), dtype=int)
+        for i, nn in enumerate(beat_intervals):
+            r_peaks[i] = nn // 2 if i == 0 else nn // 2 + np.sum(beat_intervals[:i])
+        
+        p_waves = r_peaks + np.array(self.ecg_distance.p*beat_intervals, dtype=int)
+        t_waves = r_peaks + np.array(self.ecg_distance.t*beat_intervals, dtype=int)
+        peak_inds = np.zeros(len(signal))
+        peak_inds = create_label(peak_inds, p_waves, 0.2)
+        peak_inds = create_label(peak_inds, r_peaks, 0.6)
+        peak_inds = create_label(peak_inds, t_waves, 0.4)
         
         if self.noise_generator is not None:
-            signal_, peak_inds, noise_labels = self.noise_generator.combine_signal_noise(signal_, noise_signal, peak_inds, noise_labels)       
+            signal, peak_inds, noise_labels = self.noise_generator.combine_signal_noise(signal, noise_signal, peak_inds, noise_labels)  
 
-        signal_ = signal_[:int(self.fs*self.beat_interval_generator.duration)]
+        signal = signal[:int(self.fs*self.beat_interval_generator.duration)]
         peak_inds = peak_inds[:int(self.fs*self.beat_interval_generator.duration)]
 
-        return signal_, peak_inds, noise_labels, beat_intervals/self.fs
+        return signal, peak_inds, noise_labels, beat_intervals/self.fs
     
     
-    def generate_random_set(self, number_of_signals, duration):
+    def generate_random_set(self, number_of_signals:int, duration:float):
         """
-        Generates set of random PPG signals.
+        Generates set of random ECG signals.
         
         Parameters
         ----------
@@ -114,40 +113,39 @@ class PPGGenerator(SignalGenerator):
         Returns
         ----------
         signal
-            List of clean or noisy PPG signal.
+            List of clean or noisy ECG signal.
         peak_inds
-            List of 1D array of labels for peaks and feet.
+            List of 1D array of labels for P, R and T waves.
         noise_labels
             List of noise labels: list of tuples of noise type (str) and signal-length array of amplitude of noise. Last item in the list is the artifact label (array).
         beat_list
             List of beat intervals.
         """
-        
         signals, peak_inds, noise_labels, beats_list = [], [], [], []
-        
         self.beat_interval_generator.duration = duration
         for _ in range(number_of_signals):
-            x = np.random.uniform(0, 1)
-            self.ppg_distance = self._randomize_prms(self.ppg_distance_low, self.ppg_distance_high, x)
-            self.ppg_width = self._randomize_prms(self.ppg_width_low, self.ppg_width_high, x)
-            self.ppg_amplitude = self._randomize_prms(self.ppg_amplitude_low, self.ppg_amplitude_high, x)
+            self.beat_interval_generator.beat_intervals = None
+            self.ecg_distance = self._randomize_prms(self.ecg_distance_low, self.ecg_distance_high)
+            self.ecg_width = self._randomize_prms(self.ecg_width_low, self.ecg_width_high)
+            self.ecg_amplitude = self._randomize_prms(self.ecg_amplitude_low, self.ecg_amplitude_high)
+            self.ecg_symmetry = self._randomize_prms(self.ecg_symmetry_low, self.ecg_symmetry_high)
             self.beat_interval_generator.randomize()
-        
+            
             if self.noise_generator is not None:
                 self.noise_generator.noise_type.duration = duration
                 self.noise_generator.randomize()
-
-            signal, peaks, label, beats = self.generate()
-            signals.append(signal)
-            peak_inds.append(peaks)
+            
+            signal, r_peaks, label, beats = self.generate()
+            signals.append(signal[:int(duration*self.fs)])
+            peak_inds.append(r_peaks)
             noise_labels.append(label)
             beats_list.append(beats)
         
         return signals, peak_inds, noise_labels, beats_list
 
     
-    def _randomize_prms(self, low_prms: PPGWavePrms, 
-                          high_prms: PPGWavePrms, x: float) -> np.ndarray:
+    def _randomize_prms(self, low_prms: ECGWavePrms, 
+                          high_prms: ECGWavePrms) -> np.ndarray:
         """
         Randomizes waveform parameters.
         
@@ -167,54 +165,6 @@ class PPGGenerator(SignalGenerator):
         arr = np.zeros(len(low_prms.__dataclass_fields__))
         for i, (f_low, f_high) in enumerate(zip(low_prms.__dataclass_fields__, 
                                                 high_prms.__dataclass_fields__)):
-            arr[i] = getattr(low_prms, f_low) + x*(getattr(high_prms, f_high)-getattr(low_prms, f_low))
+            arr[i] = np.random.uniform(getattr(low_prms, f_low), getattr(high_prms, f_high))
         
-        return PPGWavePrms(arr[0], arr[1])
-    
-    def gen_ppg_with_ptt(self, beat_intervals: np.ndarray, offset: float, change_offset: float, peaks_ecg: np.ndarray, idx: float) -> np.ndarray:
-        """Sets pulse transit time.
-
-        Parameters
-        ----------
-        beat_intervals
-            Beat intervals
-        offset
-            The time difference between PPG and ECG.
-        change_offset
-            The change of the time difference.
-        peaks_ecg
-            The array of the labels of the ECG peaks
-        idx
-            Index where the change happens in relation to the signal.
-        Returns
-        ----------
-        ppg
-            PPG signal
-        peaks_ppg
-            Peaks of the PPG signal
-        noise_labels
-            Noise label
-        beat_intervals_ppg
-            Beat intervals
-        """
-        x = np.linspace(-5, 5, 10)
-        mu = 0
-        var = 2
-        gaussian = 1./(np.sqrt(2.*np.pi)*var)*np.exp(-np.power((x - mu)/var, 2.)/2)
-        gaussian_cumsum = min_max_normalize(np.cumsum(gaussian), 0, change_offset)
-        gaussian = np.diff(gaussian_cumsum)
-        gaussian = gaussian + 1
-        before = np.ones(int((len(beat_intervals)-9)*idx))
-        after = np.ones(int((len(beat_intervals)-9)*(1-idx)))
-        if len(before) + len(after) + len(gaussian) != len(beat_intervals):
-            after = np.concatenate((after, np.ones(len(beat_intervals)-(len(before) + len(after) + len(gaussian)))))
-        gaussian = np.concatenate((before, gaussian, after))
-        beat_intervals = beat_intervals * gaussian
-        self.beat_interval_generator.beat_intervals = beat_intervals
-        ppg, peaks_ppg, noise_labels, beat_intervals_ppg = self.generate()
-        first_r_peak = np.argwhere(peaks_ecg == 0.6)[0]
-        first_fst_peak = np.argwhere(peaks_ppg == 0.5)[0]
-        ppg = np.roll(ppg, (first_r_peak-first_fst_peak)+(int(offset*self.fs)))
-        peaks_ppg = np.roll(peaks_ppg, (first_r_peak-first_fst_peak)+(int(offset*self.fs)))
-
-        return ppg, peaks_ppg, noise_labels, beat_intervals_ppg
+        return ECGWavePrms(arr[0], arr[1], arr[2], arr[3], arr[4])
